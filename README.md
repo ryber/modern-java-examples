8093ba50fe384e2d9b77afd8598c30cdf5461e44
Ryan Bergman <bergmanpatrickr@johndeere.com>
2024-10-18 13:37:32 -0500

    stream gathering

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # modern-java-examples
+
+## Future (Java 24+) Planned Features?
+* String Templates
+* Value Types (Project Valhalla)
+* Calling Foreign Languages (Project Panama)
+  * Warnings that JNI is bad and you should feel bad for using it
+* JVM Performance Improvements
+  * Reducing Java object headers down to under 32 bits (Project Lilliput)
+  * Enable snapshots and full restore of a JVM (project CRaC)
+  * JVM startup speed (project Leyden)
+* Reflection overhaul with the goal of supporting other programming paradigms (SQL, AI, GPUs)
---
+++ src/test/java/io/ryber/StreamGatheringTest.java
@@ -0,0 +1,53 @@
+package io.ryber;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Gatherers;
+import java.util.stream.Stream;
+
+import static java.util.List.of;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StreamGatheringTest {
+    @Test
+    void fixedWindow() {
+        var result = Stream.of(1, 2, 3, 4, 5)
+                .gather(Gatherers.windowFixed(2))
+                .toList();
+
+        assertThat(result).isEqualTo(
+                of(
+                        of(1, 2),
+                        of(3, 4),
+                        of(5)
+                )
+        );
+    }
+
+    @Test
+    void slidingWindows() {
+        var result = Stream.of(1, 2, 3, 4, 5)
+                .gather(Gatherers.windowSliding(2))
+                .toList();
+
+        assertThat(result).isEqualTo(
+                of(
+                        of(1, 2),
+                        of(2, 3),
+                        of(3, 4),
+                        of(4, 5)
+                )
+        );
+    }
+
+    @Test
+    void folding() {
+        var ints = Stream.of(1, 2, 3, 4, 5);
+
+        var result = ints
+                .gather(Gatherers.fold(() -> "", (string, number) -> string + number))
+                .findFirst();
+
+        assertThat(result.get()).isEqualTo("12345");
+    }
+}
